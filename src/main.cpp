34c8537de38295b9053addb5784591422233e8b0
SarahQuraishy <161165639+SarahQuraishy@users.noreply.github.com>
2026-05-03 20:41:37 -0400

    sarah

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+
+  // ---- LOCK ICON ----
+
+  // Lock body
+  display.fillRect(50, 30, 28, 20, SSD1306_WHITE);
+
+  // Lock shackle (top curve)
+  display.drawCircle(64, 30, 10, SSD1306_WHITE);
+
+  // Cut inside of circle to make it look like a U-shape
+  display.fillRect(54, 30, 20, 10, SSD1306_BLACK);
+
+  // Optional keyhole
+  display.fillCircle(64, 40, 2, SSD1306_BLACK);
+
+  display.display();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
